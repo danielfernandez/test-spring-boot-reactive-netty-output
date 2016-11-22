12cc502e31106f2bb92c8a287fe810491e950579
Daniel Fernández <daniel.fernandez@11thlabs.org>
2016-11-22 23:18:50 +0100

    Added  '/items/{repetitions}' controller method to output data from repository repeteated n times

--- spring-boot-reactive-netty-output-jetty/src/main/java/com/github/danielfernandez/testspringbootreactivenettyoutput/web/controller/MainController.java
+++ spring-boot-reactive-netty-output-jetty/src/main/java/com/github/danielfernandez/testspringbootreactivenettyoutput/web/controller/MainController.java
@@ -1,10 +1,23 @@
 package com.github.danielfernandez.testspringbootreactivenettyoutput.web.controller;
 
+import com.github.danielfernandez.testspringbootreactivenettyoutput.business.Item;
+import com.github.danielfernandez.testspringbootreactivenettyoutput.business.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class MainController {
+
+    private ItemRepository itemRepository = null;
+
+
+    @Autowired
+    public void setItemRepository(final ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
 
 
     @RequestMapping("/")
@@ -12,5 +25,10 @@ public class MainController {
         return "Hello from Jetty";
     }
 
+
+    @RequestMapping("/items/{repetitions}")
+    public Flux<Item> items(@PathVariable("repetitions") final int repetitions) {
+        return this.itemRepository.findAllItems(Math.max(repetitions, 1));
+    }
 
 }
