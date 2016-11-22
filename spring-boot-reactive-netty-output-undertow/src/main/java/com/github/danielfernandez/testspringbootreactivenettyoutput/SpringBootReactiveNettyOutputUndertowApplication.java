580b7c564917aa0a06bac8221e17f12324c73429
Daniel Fernández <daniel.fernandez@11thlabs.org>
2016-11-22 22:37:01 +0100

    Added Undertow-based implementation

---
+++ spring-boot-reactive-netty-output-undertow/src/main/java/com/github/danielfernandez/testspringbootreactivenettyoutput/SpringBootReactiveNettyOutputUndertowApplication.java
@@ -0,0 +1,12 @@
+package com.github.danielfernandez.testspringbootreactivenettyoutput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveNettyOutputUndertowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactiveNettyOutputUndertowApplication.class, args);
+	}
+}
