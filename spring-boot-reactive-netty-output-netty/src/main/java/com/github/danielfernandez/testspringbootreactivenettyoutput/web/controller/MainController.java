cb152ca67ca6f66cdd61ba6ebf2eb3d3125ce3ca
Daniel Fernández <daniel.fernandez@11thlabs.org>
2016-11-22 22:36:16 +0100

    Added Netty-based implementation

---
+++ spring-boot-reactive-netty-output-netty/src/main/java/com/github/danielfernandez/testspringbootreactivenettyoutput/web/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.github.danielfernandez.testspringbootreactivenettyoutput.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello from Netty";
+    }
+
+
+}
