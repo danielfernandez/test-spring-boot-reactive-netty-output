4c8427849981f5411b2db3ae84e3e67cc0e00a25
Daniel Fernández <daniel.fernandez@11thlabs.org>
2016-11-22 22:35:51 +0100

    Added Jetty-based implementation

---
+++ spring-boot-reactive-netty-output-jetty/src/main/java/com/github/danielfernandez/testspringbootreactivenettyoutput/SpringBootReactiveNettyOutputJettyApplication.java
@@ -0,0 +1,12 @@
+package com.github.danielfernandez.testspringbootreactivenettyoutput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveNettyOutputJettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactiveNettyOutputJettyApplication.class, args);
+	}
+}
